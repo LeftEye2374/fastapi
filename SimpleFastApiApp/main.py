cc91ab61297b6e543ec36981254b5e34fc394f03
LeftEye2374 <shinnn2374@mail.ru>
2026-07-29 22:09:03 +0300

    Create POST request

--- SimpleFastApiApp/main.py
+++ SimpleFastApiApp/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI,    HTTPException
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+
 import uvicorn
 app = FastAPI()
 
@@ -15,11 +17,15 @@ books = [
     },
 ]
 
-@app.get("/books")
+@app.get("/books",
+         tags=["Книги"],
+         summary="Получить все книги")
 def  read_books():
     return books
 
-@app.get("/books/{id}")
+@app.get("/books/{id}",
+         tags=["Книги"],
+         summary="Получить конкретную книгу")
 def read_book(id: int):
     for book in books:
         if book["id"] == id:
@@ -27,6 +33,19 @@ def read_book(id: int):
     raise HTTPException(status_code= 404, detail ="Книга не найдена")
 
 
+class NewBook(BaseModel):
+    title: str
+    author: str
+
+@app.post("/books",
+          tags=["Книги"],
+          summary="Добавить книгу")
+def create_book(new_book: NewBook):
+    books.append({
+        "id": len(books) + 1,
+        "title": new_book.title,
+        "author": new_book.author
+    })
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
