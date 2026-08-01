4f9c9f7a788fc870cac0cbe3fbaf792df0830634
LeftEye2374 <shinnn2374@mail.ru>
2026-08-01 14:05:08 +0300

    start work

--- fastapiWithDB/main.py
+++ fastapiWithDB/main.py
@@ -1,12 +1,10 @@
-from sqlalchemy import Column
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, Mapped
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from fastapi import FastAPI
 
 app = FastAPI()
 
-engine = create_async_engine("sqlite + aiosqlite:///books.db")
-
+engine = create_async_engine("sqlite+aiosqlite:///books.db")
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
@@ -21,7 +19,7 @@ class Base(DeclarativeBase):
 class BookModel(Base):
     __tablename__ = "books"
 
-    id : Mapped[int] = Column(primary_key=True)
+    id : Mapped[int] = mapped_column(primary_key=True)
     title : Mapped[str]
     author : Mapped[str]
 
@@ -29,5 +27,5 @@ class BookModel(Base):
 @app.post("/setup_database")
 async def setup_database():
     async with engine.begin() as connection:
-        await connection.run_async(Base.metadata.drop_all)
-        await connection.run_async(Base.metadata.create_all)
+        await connection.run_sync(Base.metadata.drop_all)
+        await connection.run_sync(Base.metadata.create_all)
