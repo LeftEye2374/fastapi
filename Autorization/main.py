148c385d0d675387e331e6e6211d76fe785e5e9f
LeftEye2374 <shinnn2374@mail.ru>
2026-08-02 15:56:11 +0300

    Add cookies

--- Autorization/main.py
+++ Autorization/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from authx import AuthX, AuthXConfig
 from pydantic import BaseModel
 
@@ -16,9 +16,10 @@ class UserLoginSchema(BaseModel):
     password : str
 
 @app.post("/login")
-def login(creds: UserLoginSchema):
+def login(creds: UserLoginSchema, response : Response):
     if creds.username == "test" and creds.password == "test":
         token = security.create_access_token(uid="12345")
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
         return {"access_token": token}
     raise HTTPException(status_code=401, detail="Incorrect username or password")
 
