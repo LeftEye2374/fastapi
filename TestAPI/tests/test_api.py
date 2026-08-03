1f0e78d522254770d9025c024306175289680430
LeftEye2374 <shinnn2374@mail.ru>
2026-08-03 16:15:57 +0300

    first API test, get

--- TestAPI/tests/test_api.py
+++ TestAPI/tests/test_api.py
@@ -1,5 +1,12 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
+from main import app
 
+
+@pytest.mark.asyncio
 async def test_get_books():
-    ...
+    async with AsyncClient(transport=ASGITransport(app = app),
+                           base_url="htpp://test",
+                           ) as cl:
+        response = await cl.get("/books")
+        print(response)
